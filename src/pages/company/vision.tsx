a472a6e22edd4281a2a60cf752371285a48415d8
Halil Teyfik Dolmaci <hlltvfk@gmail.com>
2023-07-14 20:52:35 +0300

    added vision

---
+++ src/pages/company/vision.tsx
@@ -0,0 +1,30 @@
+import Layout from '@/components/Layout';
+import pageWithTranslations from '@/helpers/pageWithTranslations';
+import { Container } from '@mantine/core';
+import { useTranslation } from 'teyfik-i18n-next';
+
+export default function Vision() {
+  const { t } = useTranslation();
+
+  return (
+    <Layout>
+      <Container>
+        <section className="flex mt-12 gap-8 justify-between">
+          <div className="w-full md:max-w-xl">
+            <header>
+              <h2 className="text-3xl">{t('vision.title')}</h2>
+            </header>
+
+            <br />
+
+            <main>
+              <p className="whitespace-pre-wrap">{t('vision.description')}</p>
+            </main>
+          </div>
+        </section>
+      </Container>
+    </Layout>
+  );
+}
+
+export const getStaticProps = pageWithTranslations('common vision');
